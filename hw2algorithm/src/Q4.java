46725d641191e9bd3f4550833116678b57fbdb26
Parmiss <parmiss.sh98@gmail.com>
2018-10-15 20:45:00 +0330

    accepted

--- hw2algorithm/src/Q4.java
+++ hw2algorithm/src/Q4.java
@@ -12,42 +12,39 @@ public class Q4 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         ArrayList<String> a = new ArrayList<>();
+        int n;
+        ArrayList<Runner> runners = new ArrayList<>();
+
+        PriorityQueue<Runner> pq = new PriorityQueue<>();
         while (true) {
-            String k = s.nextLine();
+            String k = s.next();
             if (k.equals("#")) {
                 break;
             }
-            a.add(k);
-        }
-        int n;
-        ArrayList<Runner> runners = new ArrayList<>();
-        n = s.nextInt();
-        for (String k : a) {
-            String[] splitedk = k.split(" ");
-            String name = splitedk[0];
-            // System.out.println(k);
-            int time = Integer.parseInt(splitedk[1]);
-            int additionalTime = Integer.parseInt(splitedk[2]);
-            Runner runner = new Runner(name, time, additionalTime);
+            String name=k;
+            int time=s.nextInt();
+            int addingTime=s.nextInt();
+           Runner runner = new Runner(name, time, addingTime);
             runners.add(runner);
-        }
+            pq.add(runner);
 
-        PriorityQueue<Runner> pq = new PriorityQueue<>();
-
-        for (int j = 0; j < runners.size(); j++) {
-            runners.get(j).totalTime();
-           // runners.get(j).round++;
-            pq.add(runners.get(j));
         }
+        n = s.nextInt();
+
+
+
+
         for (int i = 1; i < n; i++) {
             Runner r = pq.remove();
            // r.round++;
             System.out.println(r.name);
+           // System.out.println("r.totaltime = " + r.totaltime);
             //System.out.println(r.totaltime);
             r.totalTime();
             pq.add(r);
         }
-        System.out.println(pq.remove().name);
+        System.out.print(pq.remove().name);
+       // System.out.println("pq.remove()totaltime = " + pq.remove().totaltime);
         //System.out.println(pq.remove().totaltime);
 
     }
@@ -63,6 +60,7 @@ public class Q4 {
             this.name = name;
             this.time = time;
             this.addingtime = addingtime;
+            totaltime=time;
         }
 
         public int totalTime() {
@@ -77,10 +75,9 @@ public class Q4 {
                 return -1;
             if (this.totaltime == o.totaltime){
                 if(this.name.compareTo(o.name)<0)
-                  return 0;
+                  return -1;
                 else
                     return 1;
-
 
             }
 
